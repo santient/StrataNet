0d7dff209af5d6ce742d3aec74e65dc258bc3a0f
Santiago Benoit <santib27@gmail.com>
2020-04-08 23:45:31 -0400

    scale by kernel pdf, support different query and key/value lengths

--- StrataNet/sparse_attn.py
+++ StrataNet/sparse_attn.py
@@ -46,13 +46,57 @@ from torch import nn
 #         out = self.out_ff(torch.stack([attn @ val for attn, val in zip(attn_matrix, value)]).transpose(0, 1).contiguous().view(n, -1, self.embed_dim))
 #         return out
 
+# class SparseKernelMultiheadAttention(nn.Module):
+#     def __init__(self, embed_dim, num_heads, distribution, num_samples=1000):
+#         super(SparseKernelMultiheadAttention, self).__init__()
+#         self.embed_dim = embed_dim
+#         self.num_heads = num_heads
+#         self.distribution = distribution
+#         self.num_samples = num_samples
+#         self.head_dim = embed_dim // num_heads
+#         if self.head_dim * num_heads != self.embed_dim:
+#             raise ValueError("embed_dim must be divisible by num_heads")
+#         self.query_ff = nn.Linear(embed_dim, embed_dim)
+#         self.key_ff = nn.Linear(embed_dim, embed_dim)
+#         self.value_ff = nn.Linear(embed_dim, embed_dim)
+#         self.out_ff = nn.Linear(embed_dim, embed_dim)
+#         self._reset_parameters()
+
+#     def _reset_parameters(self):
+#         for p in self.parameters():
+#             if p.dim() > 1:
+#                 nn.init.xavier_uniform_(p)
+
+#     def forward(self, query, key, value, **kwargs):
+#         # pytorch sparse tensors still under active development, so expect changes soon
+#         n = query.size(0)
+#         device = query.device
+#         query = self.query_ff(query).view(n, -1, self.head_dim).transpose(0, 1)
+#         key = self.key_ff(key).view(n, -1, self.head_dim).transpose(0, 1)
+#         value = self.value_ff(value).view(n, -1, self.head_dim).transpose(0, 1)
+#         samples = self.distribution.sample((n, self.num_samples)).round().long()
+#         samples = (samples + torch.arange(n).unsqueeze(1)).clamp(0, n - 1)
+#         cols = [samples[i].unique().to(device) for i in range(n)]
+#         del samples
+#         rows = [torch.tensor(i, device=device).repeat(cols[i].size(0)) for i in range(n)]
+#         vals = [torch.softmax((query[:, rows[i], :] * key[:, cols[i], :]).sum(-1) / math.sqrt(n), dim=-1) for i in range(n)]
+#         rows = torch.cat(rows)
+#         cols = torch.cat(cols)
+#         vals = torch.cat(vals, dim=-1)
+#         idxs = torch.stack([rows, cols])
+#         del rows, cols, query, key
+#         attn_matrix = [torch.sparse.FloatTensor(idxs, val, (n, n)) for val in vals]
+#         out = self.out_ff(torch.stack([attn @ val for attn, val in zip(attn_matrix, value)]).transpose(0, 1).contiguous().view(n, -1, self.embed_dim))
+#         return out
+
 class SparseKernelMultiheadAttention(nn.Module):
-    def __init__(self, embed_dim, num_heads, distribution, num_samples=1000):
+    def __init__(self, embed_dim, num_heads, distribution, dropout=0.1, attn_span=100):
         super(SparseKernelMultiheadAttention, self).__init__()
         self.embed_dim = embed_dim
         self.num_heads = num_heads
         self.distribution = distribution
-        self.num_samples = num_samples
+        self.dropout = dropout
+        self.attn_span = attn_span
         self.head_dim = embed_dim // num_heads
         if self.head_dim * num_heads != self.embed_dim:
             raise ValueError("embed_dim must be divisible by num_heads")
@@ -69,22 +113,23 @@ class SparseKernelMultiheadAttention(nn.Module):
 
     def forward(self, query, key, value, **kwargs):
         # pytorch sparse tensors still under active development, so expect changes soon
-        n = query.size(0)
-        device = query.device
-        query = self.query_ff(query).view(n, -1, self.head_dim).transpose(0, 1)
+        m = query.size(0)
+        n = key.size(0)
+        if key.size(0) != value.size(0):
+            raise RuntimeError("key and value must have same length")
+        query = self.query_ff(query).view(m, -1, self.head_dim).transpose(0, 1)
         key = self.key_ff(key).view(n, -1, self.head_dim).transpose(0, 1)
+        key = torch.cat([key, torch.zeros(key.size(0), 1, key.size(2), device=key.device)], dim=1)
         value = self.value_ff(value).view(n, -1, self.head_dim).transpose(0, 1)
-        samples = self.distribution.sample((n, self.num_samples)).round().long()
-        samples = (samples + torch.arange(n).unsqueeze(1)).clamp(0, n - 1)
-        cols = [samples[i].unique().to(device) for i in range(n)]
-        del samples
-        rows = [torch.tensor(i, device=device).repeat(cols[i].size(0)) for i in range(n)]
-        vals = [torch.softmax((query[:, rows[i], :] * key[:, cols[i], :]).sum(-1) / math.sqrt(n), dim=-1) for i in range(n)]
-        rows = torch.cat(rows)
-        cols = torch.cat(cols)
-        vals = torch.cat(vals, dim=-1)
+        value = torch.cat([value, torch.zeros(value.size(0), 1, value.size(2), device=value.device)], dim=1)
+        rows = torch.arange(m, device=query.device).repeat(2 * self.attn_span + 1, 1).transpose(0, 1).flatten()
+        cols = torch.cat([torch.arange(i - self.attn_span, i + self.attn_span + 1, device=query.device) for i in range(n)])
+        out_of_bounds = (cols < 0) | (cols >= n)
+        cols[out_of_bounds] = n
         idxs = torch.stack([rows, cols])
-        del rows, cols, query, key
-        attn_matrix = [torch.sparse.FloatTensor(idxs, val, (n, n)) for val in vals]
+        vals = (self.distribution.log_prob(rows - cols).unsqueeze(0) + (query[:, rows, :] * key[:, cols, :]).sum(-1)) / math.sqrt(n)
+        vals[:, out_of_bounds] = -float("inf")
+        vals = torch.dropout(torch.softmax(vals.view(-1, n, 2 * self.attn_span + 1), dim=-1), self.dropout, self.training).view(-1, idxs.size(1))
+        attn_matrix = [torch.sparse.FloatTensor(idxs, val, (m, n + 1)) for val in vals]
         out = self.out_ff(torch.stack([attn @ val for attn, val in zip(attn_matrix, value)]).transpose(0, 1).contiguous().view(n, -1, self.embed_dim))
         return out
